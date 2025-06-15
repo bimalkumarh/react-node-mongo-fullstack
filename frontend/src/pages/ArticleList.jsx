e1148edc760f038fd53b5ca76bb3397e7f02c939
Bimal Kumar H <bimalkumarh@gmail.com>
2025-06-15 09:19:04 +0530

    added pages and routing

---
+++ frontend/src/pages/ArticleList.jsx
@@ -0,0 +1,8 @@
+export default function ArticleListPage() {
+    return (
+        <div>
+            <h1>Article List</h1>
+            <p>This is the main page of our application.</p>
+        </div>
+    );
+}
---
+++ frontend/src/pages/ArticlePage.jsx
@@ -0,0 +1,8 @@
+export default function ArticlePage() {
+    return (
+        <div>
+            <h1>Article Page</h1>
+            <p>This is the main page of our application.</p>
+        </div>
+    );
+}
